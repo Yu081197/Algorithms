84ad2d0192570c0cce8048446b6947199017c554
Dasong Yu <81637903+Yu081197@users.noreply.github.com>
2023-04-21 15:51:45 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 520 ms, Memory: 60704 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,22 @@
+// fs 모듈을 이용해 파일 전체를 읽어와 문자열로 저장하기
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let data = [];
+for (let i = 1; i <= n; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  data.push([x, y]);
+}
+
+function compare(a, b) {
+  if (a[1] != b[1]) return a[1] - b[1]; // x 좌표 기준 오름차순
+  else return a[0] - b[0]; // x가 같으면 y 좌표 기준 오름차순
+}
+data.sort(compare);
+
+let answer = "";
+for (let point of data) {
+  answer += point[0] + " " + point[1] + "\n";
+}
+console.log(answer);
