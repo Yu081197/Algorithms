224107619b12f2c8460cd84c67d598f83683e097
dasong <sonaki970811@gmail.com>
2023-06-01 09:11:51 +0900

    2023/06/01

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ sys.stdin = open("input.txt", "r")
 
 tc = int(input())
 for _ in range(tc):
-    n, m = list(map(int, input().split(' ')))
-    queue = list(map(int, input().split(' '))) # [2, 1, 4, 3]
-    queue = [(i, idx) for idx, i in enumerate(queue)] # enumerate는 인덱스와 함께 튜플로 저장하는 함수 => [(2, 0),(1, 1),(4, 2),(3, 3)]
-    count = 0
-    
-    while  True:
-        if queue[0][0] == max(queue, key=lambda x: x[0])[0]: # queue에 대해서 x[0]의 값이 가장 큰 원소를 뽑아온다는 뜻. 그것이 queue[0][0]과 같다면
-            count += 1
-            if queue[0][1] == m:
-                print(count)
-                break
-            else:
-                queue.pop(0)
+    data = input()
+    left_stack = []
+    right_stack = []
+    for i in data:
+        if i == '-':
+            if left_stack:
+                left_stack.pop()
+        elif i == '<':
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif i == '>':
+            if right_stack:
+                left_stack.append(right_stack.pop())
         else:
-            queue.append(queue.pop(0))
+            left_stack.append(i)
+            
+    left_stack.extend(reversed(right_stack))
+    print(''.join(left_stack))
