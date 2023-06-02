c74122cb293e4be1aa573b5bf2ebb4049585a6c0
dasong <sonaki970811@gmail.com>
2023-06-02 14:09:16 +0900

    2023/06/02

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 k = int(input())
+stack = []
+
+for _ in range(k):
+    x = int(input())
+    if x == 0:
+        stack.pop()
+    else :
+        stack.append(x)
+        
+print(sum(stack))
