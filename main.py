f7b538d11f1b67f2866f90b6b26102827dd3d051
dasong <sonaki970811@gmail.com>
2023-06-02 16:19:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 from collections import deque
 import sys
 
-
-
 sys.stdin = open("input.txt", "r")
-
-k = int(input())
-stack = []
-
-for _ in range(k):
-    x = int(input())
-    if x == 0:
-        stack.pop()
-    else :
-        stack.append(x)
-        
-print(sum(stack))
