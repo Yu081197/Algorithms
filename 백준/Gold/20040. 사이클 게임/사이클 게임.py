a30f915a0d98afdbb7d9fbffec01910be09a0c64
Dasong Yu <81637903+Yu081197@users.noreply.github.com>
2023-06-16 16:20:10 +0900

    [Gold IV] Title: 사이클 게임, Time: 840 ms, Memory: 51708 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * n
+
+
+for i in range(n):
+    parent[i] = i
+
+cycle = False
+for i in range(m):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        print(i + 1)
+        break
+    else:
+        union_parent(parent, a, b)
+
+if not cycle:
+    print(0)
