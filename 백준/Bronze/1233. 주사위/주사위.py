fd362d38ba8408604412963bbadcdba90dc2035d
Dasong Yu <81637903+Yu081197@users.noreply.github.com>
2023-06-18 23:08:24 +0900

    [Bronze II] Title: 주사위, Time: 80 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+s1, s2, s3 = map(int, input().split())
+result = []
+
+for i in range(1, s1 + 1):
+    for j in range(1, s2 + 1):
+        for k in range(1, s3 + 1):
+            summary = i + j + k
+            result.append(summary)
+result.sort()
+
+
+s = list(set(result))
+cnt = []
+for i in s:
+    cnt.append(result.count(i))
+print(s[cnt.index(max(cnt))])
