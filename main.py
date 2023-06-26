98bb2476e62c1e2ef553107ea6a90c48b6173474
dasong <sonaki970811@gmail.com>
2023-06-26 16:50:08 +0900

    2023/06/26

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import sys
 from collections import deque
-
+sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-sys.stdin = open("input.txt", "r")
 
-n, m = map(int, input().split())  # 원소의 개수, 뽑아내는 횟수
-d = deque([i for i in range(1, n+1)])  # 1부터 N까지의 원소를 삽입
-targets = list(map(int, input().split()))  # 뽑아낼 원소 목록
-cnt = 0  # 회전 연산 수행 횟수
 
-for target in targets:  # 뽑아낼 원소를 하나씩 확인하며
-    index = d.index(target)  # 덱에서 해당 원소의 위치를 찾기
-    if index <= len(d) // 2:  # 왼쪽으로 돌리는 게 더 빠른 경우
-        for i in range(index):  # 회전 연산 반복 수행
-            x = d.popleft()
-            d.append(x)
-            cnt += 1
-    else:  # 오른쪽으로 돌리는 게 더 빠른 경우
-        for i in range(len(d) - index):  # 회전 연산 반복 수행
-            x = d.pop()
-            d.appendleft(x)
-            cnt += 1
-    d.popleft()  # 원소 꺼내기
+n = int(input())
+lectures = []
 
-print(cnt)
+
+for i in range(n):
+    id, start, end = map(int, input().split())
