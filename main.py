ac47772fa91cfeea0ddced946b6565146e51bc5f
dasong <sonaki970811@gmail.com>
2023-06-26 17:22:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import sys
 from collections import deque
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
-
-
+import heapq
 
 n = int(input())
 lectures = []
@@ -11,3 +10,14 @@ lectures = []
 
 for i in range(n):
     id, start, end = map(int, input().split())
+    heapq.heappush(lectures, (start, end))
+
+heap = []
+end = heapq.heappop(lectures)[1]
+heapq.heappush(heap, end)
+answer = 1
+
+for i in range(n-1):
+    new_start, new_end, heapq.heappop(lectures)
+    end = heapq.heappop(heap)
+    
