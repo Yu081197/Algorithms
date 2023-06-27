2e3b3a629180022a4cf09f72ebb6b30650cf078d
Dasong Yu <81637903+Yu081197@users.noreply.github.com>
2023-06-27 16:41:19 +0900

    [Gold V] Title: 강의실, Time: 420 ms, Memory: 46740 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -3,26 +3,26 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
+n = int(input())  # 강의 개수 입력받기
 lectures = []
 for i in range(n):
-    id, start, end = map(int, input().split())
-    heapq.heappush(lectures, (start, end))
+    id, start, end = map(int, input().split())  # 강의번호, 시작시간, 종료시간 입력
+    heapq.heappush(lectures, (start, end))  # 시작시간 종료시간을 기준으로 힙에 삽입
 
-heap = []
-end = heapq.heappop(lectures)[1]
+heap = []  # 배정된 강의실
+end = heapq.heappop(lectures)[1]  # 첫 번째 강의가 끝나는 시간을 우선순위 큐에 삽입
 heapq.heappush(heap, end)
-answer = 1
+answer = 1  # 필요한 강의실 개수
 
-for i in range(n-1):
-    new_start, new_end = heapq.heappop(lectures)
-    end = heapq.heappop(heap)
+for i in range(n - 1):
+    new_start, new_end = heapq.heappop(lectures)  # 강의 목록에서 [새 강의] 꺼내기
+    end = heapq.heappop(heap)  # [기존 강의] 중에서 가장 일찍 끝나는 강의 꺼내기
 
-    if new_start < end:
-        heapq.heappush(heap, end)
-        heapq.heappush(heap, new_end)
+    if new_start < end:  # 강의시간이 만약 겹친다면?
+        heapq.heappush(heap, end)  # 기존 강의실은 유지
+        heapq.heappush(heap, new_end)  # 새로운 강의실 추가
         answer += 1
-    else:
-        heapq.heappush(heap, new_end)
+    else:  # 겹치지 않는다면
+        heapq.heappush(heap, new_end)  # 기존 강의실에 배정
 
 print(answer)
