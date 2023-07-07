e4a926dee9b7879fb897e269c7dd988dd2ee6e3a
Dasong Yu <81637903+Yu081197@users.noreply.github.com>
2023-07-07 23:49:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.50 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+         # 순서가 맞지않으면 완주하지 못한 선수
+        if participant[i] != completion[i]:
+            return participant[i]
+    # 위에서 순서가 다 맞았다면 마지막 선수가 완주하지 못한 선수        
+    return participant[-1]
+    answer = ''
+    return answer
