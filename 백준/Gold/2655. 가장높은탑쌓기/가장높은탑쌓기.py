0267e350808c2d8fcc32021185042fab68f8dce6
Dasong Yu <81637903+Yu081197@users.noreply.github.com>
2023-07-13 16:11:14 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.py
@@ -0,0 +1,35 @@
+
+import sys
+
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+dp = [0] * (n + 1)
+
+arr.append((0,0,0,0))
+
+for i in range(1, n + 1):
+    area, height, weight = map(int, input().split())
+    arr.append((i, area, height, weight))
+
+arr.sort(key=lambda data: data[3])
+for i in range(1, n + 1):
+    for j in range(0, i):
+        if arr[i][1] > arr[j][1]:
+            dp[i] = max(dp[i],dp[j] + arr[i][2])
+
+max_value = max(dp)
+index = n
+result = []
+
+while index != 0:
+    if max_value == dp[index]:
+        result.append(arr[index][0])
+        max_value -= arr[index][2]
+    index -= 1
+
+result.reverse()
+print(len(result))
+[print(i) for i in result]
